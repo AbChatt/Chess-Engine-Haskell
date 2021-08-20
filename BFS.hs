88499f6f3ef53ee382cdec35e5efe444d0ad23b0
AbChatt <abhi1506@live.com>
2021-08-19 23:24:59 -0400

    Added base code and handout

---
+++ BFS.hs
@@ -0,0 +1,23 @@
+module BFS where
+
+-- Question 1
+
+knightNext :: Int -> (Int, Int) -> [(Int, Int)]
+knightNext n (x,y) = error "TODO"
+
+
+-- The quad function as in the handout
+quad :: Int -> [Int]
+quad i = [4*i + 1, 4*i + 2, 4*i + 3, 4*i + 4]
+
+
+-- Question 2
+
+bfs :: (a -> [a]) -> a -> [a]
+bfs next x0 = error "TODO"
+
+
+-- Question 3
+
+iterDeep :: (a -> [a]) -> a -> [a]
+iterDeep next x0 = error "TODO"
